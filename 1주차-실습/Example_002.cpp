7d6ff72b9a21b33b868cf216db7e9cf09bb8f82b
nhk9680 <30471027+nhk9680@users.noreply.github.com>
2019-02-08 20:27:13 +0900

    Update Example_002.cpp
    add remarks - please add printf to see when processing cmd also.

--- 1주차-실습/Example_002.cpp
+++ 1주차-실습/Example_002.cpp
@@ -10,7 +10,7 @@
 
 using namespace cv;
 /////////////////////////////////////////////
-//*****사진의 개수를 설정해주세요.*****
+//*****please setting the number of images*****
 #define NUM 3
 /////////////////////////////////////////////
 
@@ -20,13 +20,16 @@ int main() {
 	Mat result;
 	Mat imgArray[NUM];
 
+	//input images
 	for (int i = 0; i < NUM; i++)
 	{
-		imgArray[i] = imread("./images/" + std::to_string(i+1) + ".jpg");
+		imgArray[i] = imread("./images/" + std::to_string(i + 1) + ".jpg");
 	}
-	
+
+	//Panormama stitching func
 	result = MakePano(imgArray, NUM);
 
+	//shows image
 	imshow("result", result);
 
 	waitKey();
@@ -35,70 +38,84 @@ int main() {
 
 Mat MakePano(Mat *imgArray, int num)
 {
+	//panorama base
 	Mat mainPano = imgArray[0];
+
+	//loop while stitching all images
 	for (int i = 1; i < num; i++)
 	{
+		//convert image to gray : computing faster
 		Mat gray_mainImg, gray_objImg;
 		cvtColor(mainPano, gray_mainImg, COLOR_RGB2GRAY);
 		cvtColor(imgArray[i], gray_objImg, COLOR_RGB2GRAY);
 
+		//SIFT detect algorithm
 		SiftFeatureDetector detector(0.3);
 		vector<KeyPoint> point1, point2;
-
-		Mat pointmat1, pointmat2;
 		detector.detect(gray_mainImg, point1);
 		detector.detect(gray_objImg, point2);
 
-
+		//Descriptor
 		SiftDescriptorExtractor extractor;
 		Mat descriptor1, descriptor2;
-		extractor.compute(gray_mainImg, point1, pointmat1);
-		extractor.compute(gray_objImg, point2, pointmat2);
+		extractor.compute(gray_mainImg, point1, descriptor1);
+		extractor.compute(gray_objImg, point2, descriptor2);
 
+		//match keypoints
 		FlannBasedMatcher matcher;
 		vector<DMatch> matches;
+		matcher.match(descriptor1, descriptor2, matches);
 
-		matcher.match(pointmat1, pointmat2, matches);
+		//get minimal distance
 		double mindistance = 5000;
 		double distance;
-		for (int i = 0; i < pointmat1.rows; i++) {
+		for (int i = 0; i < descriptor1.rows; i++) {
 			distance = matches[i].distance;
 			if (mindistance > distance)mindistance = distance;
 		}
 
+		//filtering matches using minimal distance
 		vector<DMatch>goodmatch;
-		for (int i = 0; i < pointmat1.rows; i++) {
+		for (int i = 0; i < descriptor1.rows; i++) {
 			if (matches[i].distance < 5 * mindistance)
 				goodmatch.push_back(matches[i]);
 		}
 
+		//drawing lines between matched keypoints
 		Mat matGoodMatcges;
 		drawMatches(mainPano, point1, imgArray[i], point2, goodmatch, matGoodMatcges, Scalar::all(-1), Scalar(-1), vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
 
-		imshow("after drawing matches "+std::to_string(i), matGoodMatcges);
+		//(option)shows the image
+		imshow("after drawing matches " + std::to_string(i), matGoodMatcges);
+
 
 		vector<Point2f> obj;
 		vector<Point2f> scene;
-		for (int i = 0; i < goodmatch.size(); i++) {
+		for (unsigned int i = 0; i < goodmatch.size(); i++) {
 			obj.push_back(point1[goodmatch[i].queryIdx].pt);
 			scene.push_back(point2[goodmatch[i].trainIdx].pt);
 		}
 
+		//Find Homography
 		Mat homomatrix = findHomography(scene, obj, CV_RANSAC);
 
 		//////////////////////////////////////////////////////
 		////////////////////stitching part////////////////////
 		//////////////////////////////////////////////////////
 
+		//warp images
 		Mat warp;
 		warpPerspective(imgArray[i], warp, homomatrix, Size(imgArray[i].cols + mainPano.cols, imgArray[i].rows), INTER_CUBIC);
 
+		//copy warped image
 		Mat matPanorama;
 		matPanorama = warp.clone();
 
+		//paste
 		Mat matROI(matPanorama, Rect(0, 0, mainPano.cols, mainPano.rows));
 		mainPano.copyTo(matROI);
 
+		//
 		vector<Point> nonBlackList;
 		nonBlackList.reserve(warp.rows *warp.cols);
 
@@ -114,7 +131,7 @@ Mat MakePano(Mat *imgArray, int num)
 			Mat img = matPanorama(Range(0, matPanorama.rows), Range(0, max));
 			mainPano = img;
 		}
-		imshow("after stitching "+std::to_string(i), mainPano);
+		imshow("after stitching " + std::to_string(i), mainPano);
 
 	}
 
