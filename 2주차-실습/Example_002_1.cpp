bf2dc1b4ac2e9263d1c1cbf78ed2ec0933e777f5
nhk9680 <30471027+nhk9680@users.noreply.github.com>
2019-02-16 16:03:06 +0900

    modify dir
    filename이 data~로 시작합니다.

--- 2주차-실습/Example_002_1.cpp
+++ 2주차-실습/Example_002_1.cpp
@@ -56,7 +56,7 @@ int main()
 		cout << num << " image load" << endl;
 		for (int i = NUMBER_train; i < NUMBER_test+ NUMBER_train; i++)
 		{
-			Mat MNIST = imread("./data2/MNIST" + std::to_string(num) + "_" + std::to_string(i + 1) + ".PNG", COLOR_RGB2GRAY);
+			Mat MNIST = imread("./data2/MNIST/data " + std::to_string(num) + "_" + std::to_string(i + 1) + ".PNG", COLOR_RGB2GRAY);
 			if (!(MNIST.data))
 			{
 				cout << "image load fail" << endl;
