e2a00da5cba7ad666877ab1d9577c29697d87a05
김지원 <44772344+socome@users.noreply.github.com>
2019-02-16 21:11:01 +0900

    Update Example_002_1.cpp

--- 2주차-실습/Example_002_1.cpp
+++ 2주차-실습/Example_002_1.cpp
@@ -1,4 +1,3 @@
-
 #include <opencv2/opencv.hpp>
 #include <opencv2/objdetect.hpp>
 #include <opencv2/ml.hpp>
@@ -22,8 +21,8 @@ vector<float> find_HOG_feature_image(Mat img);
 int main()
 {
 
-	vector<vector<float>> MNIST_train_HOG, MNIST_test_HOG;
-	vector<int> MNIST_train_label, MNIST_test_label;
+	vector<vector<float>> HOG_train_data,  HOG_test_data;
+	vector<int> HOG_train_data_label, HOG_test_data_label;
 
 	cout << "\n//////////////////////////////////////////////////////" << endl;
 
@@ -34,16 +33,14 @@ int main()
 		cout << num << " image load" << endl;
 		for (int i = 0; i < NUMBER_train; i++)
 		{
-			Mat MNIST = imread("./data2/MNIST/data "+ std::to_string(num) + "_" +std::to_string(i + 1) + ".PNG",COLOR_RGB2GRAY);
+			Mat MNIST = imread("./MNIST/data "+ std::to_string(num) + "_" +std::to_string(i + 1) + ".PNG",COLOR_RGB2GRAY);
 			if (!(MNIST.data))
 			{
 				cout << "image load fail" << endl;
 				return 0;
 			}
-			//imshow("train", MNIST);
-			//waitKey(1);
-			MNIST_train_HOG.push_back(find_HOG_feature_image(MNIST));
-			MNIST_train_label.push_back(num);
+			HOG_train_data.push_back(find_HOG_feature_image(MNIST));
+			HOG_train_data_label.push_back(num);
 		}
 
 	}
@@ -54,96 +51,120 @@ int main()
 	for (int num = 0; num < 10; num++)
 	{
 		cout << num << " image load" << endl;
-		for (int i = NUMBER_train; i < NUMBER_test+ NUMBER_train; i++)
+		for (int i = NUMBER_train; i < NUMBER_test + NUMBER_train; i++)
 		{
-			Mat MNIST = imread("./data2/MNIST/data " + std::to_string(num) + "_" + std::to_string(i + 1) + ".PNG", COLOR_RGB2GRAY);
+			Mat MNIST = imread("./MNIST/data " + std::to_string(num) + "_" + std::to_string(i + 1) + ".PNG", COLOR_RGB2GRAY);
 			if (!(MNIST.data))
 			{
 				cout << "image load fail" << endl;
 				return 0;
 			}
-			//imshow("test", MNIST);
-			//waitKey(1);
-			MNIST_test_HOG.push_back(find_HOG_feature_image(MNIST));
-			MNIST_test_label.push_back(num);
+			HOG_test_data.push_back(find_HOG_feature_image(MNIST));
+			HOG_test_data_label.push_back(num);
 		}
 
 	}
-	cout << "\n//////////////////////////////////////////////////////" << endl;
-
-	cout << "                      svm start                      " << endl;
-
-	cout << "\n//////////////////////////////////////////////////////\n" << endl;
-
-
-	int descriptor_size = MNIST_train_HOG[0].size();
-	Mat MNIST_train_HOG_Mat(MNIST_train_HOG.size(), descriptor_size, CV_32FC1);
-	Mat MNIST_test_HOG_Mat(MNIST_test_HOG.size(), descriptor_size, CV_32FC1);
-	ConvertVectortoMatrix(MNIST_train_HOG, MNIST_test_HOG, MNIST_train_HOG_Mat, MNIST_test_HOG_Mat);
-
 
 	cout << "\n//////////////////////////////////////////////////////" << endl;
 
+
+	int descriptor_size = HOG_train_data[0].size();
+
+	Mat HOG_train_data_Mat(HOG_train_data.size(), descriptor_size, CV_32FC1);
+	Mat HOG_test_data_Mat(HOG_test_data.size(), descriptor_size, CV_32FC1);
+	Mat HOG_train_data_label_Mat(HOG_train_data_label.size(), 1, CV_32FC1);
+	Mat HOG_test_data_label_Mat(HOG_test_data_label.size(), 1, CV_32FC1);
+
+	ConvertVectortoMatrix(HOG_train_data, HOG_test_data, HOG_train_data_Mat, HOG_test_data_Mat);
+
+	for (int i = 0; i < HOG_train_data_label.size(); i++)
+	{
+		HOG_train_data_label_Mat.at<float>(i, 0) = HOG_train_data_label[i];
+	}
+	for (int i = 0; i < HOG_test_data_label.size(); i++)
+	{
+		HOG_test_data_label_Mat.at<float>(i, 0) = HOG_test_data_label[i];
+	}
+
+
+	cout << "\n//////////////////////////////////////////////////////" << endl;
 	cout << "                      svm setting                      " << endl;
+	cout << "//////////////////////////////////////////////////////\n" << endl;
 
-	cout << "\n//////////////////////////////////////////////////////\n" << endl;
-
-	CvSVMParams params;
-	params.svm_type = CvSVM::C_SVC;
-	params.kernel_type = CvSVM::RBF;
-	params.gamma = 0.50625;
-	params.C = 2.5;
 	CvSVM svm;
 
+	CvSVMParams params = CvSVMParams
+    (
+	  CvSVM::C_SVC,   // Type of SVM, here N classes (see manual)
+	  CvSVM::LINEAR,  // kernel type (see manual)
+	  0.0,            // kernel parameter (degree) for poly kernel only
+	  0.0,            // kernel parameter (gamma) for poly/rbf kernel only
+	  0.0,            // kernel parameter (coef0) for poly/sigmoid kernel only
+	  10,             // SVM optimization parameter C
+	  0,              // SVM optimization parameter nu (not used for N classe SVM)
+	  0,              // SVM optimization parameter p (not used for N classe SVM)
+	  NULL,           // class wieghts (or priors)
+	  // Optional weights, assigned to particular classes.
+	  // They are multiplied by C and thus affect the misclassification
+	  // penalty for different classes. The larger weight, the larger penalty
+	  // on misclassification of data from the corresponding class.
 
-	CvMat tryMat = MNIST_train_HOG_Mat;
-	Mat trainLabelsMat(MNIST_train_label.size(), 1, CV_32FC1);
+	  // termination criteria for learning algorithm
 
-	for (int i = 0; i < MNIST_train_label.size(); i++)
-	{
-		trainLabelsMat.at<float>(i, 0) = MNIST_train_label[i];
-	}
+	  cvTermCriteria(CV_TERMCRIT_ITER + CV_TERMCRIT_EPS, 1000, 0.000001)
+
+   );
 
 	cout << "\n//////////////////////////////////////////////////////" << endl;
-
 	cout << "                      svm train                      " << endl;
+	cout << "//////////////////////////////////////////////////////\n" << endl;
 
-	cout << "\n//////////////////////////////////////////////////////\n" << endl;
-
-	CvMat tryMat_2 = trainLabelsMat;
-	svm.train(&tryMat, &tryMat_2, Mat(), Mat(), params);
+	svm.train_auto(HOG_train_data_Mat, HOG_train_data_label_Mat, Mat(), Mat(), params, 10);
 
 	cout << "\n//////////////////////////////////////////////////////" << endl;
-
 	cout << "                      svm save                   " << endl;
+	cout << "//////////////////////////////////////////////////////\n" << endl;
 
-	cout << "\n//////////////////////////////////////////////////////\n" << endl;
-	/// Save svm file
 	svm.save("MNIST_HOG_SVM.xml");
-	Mat testResponse;
 
 	cout << "\n//////////////////////////////////////////////////////" << endl;
-
 	cout << "                      svm predict                      " << endl;
-
-	cout << "\n//////////////////////////////////////////////////////\n" << endl;
-
-	svm.predict(MNIST_test_HOG_Mat, testResponse);
-
-	float count = 0, accuracy = 0;
-	for (int i = 0; i < testResponse.rows; i++)
+	cout << "//////////////////////////////////////////////////////\n" << endl;
+	
+	Mat Response_test;
+	Mat Response_train;
+	
+	svm.predict(HOG_train_data_Mat, Response_train);
+	float count_train = 0, accuracy_train = 0;
+	for (int i = 0; i < Response_train.rows; i++)
 	{
-		if (testResponse.at<float>(i, 0) == MNIST_test_label[i])
+		if (Response_train.at<float>(i, 0) == HOG_train_data_label[i])
 		{
-			count = count + 1;
+			count_train = count_train + 1;
 		}
 	}
 
-	accuracy = (count / testResponse.rows) * 100;
+	svm.predict(HOG_test_data_Mat, Response_test);
+	float count_test = 0, accuracy_test = 0;
+	for (int i = 0; i < Response_test.rows; i++)
+	{
+		if (Response_test.at<float>(i, 0) == HOG_test_data_label[i])
+		{
+			count_test = count_test + 1;
+		}
+	}
+
+	accuracy_train = (count_train / Response_train.rows) * 100;
+	cout << "accuracy_train : " << accuracy_train << endl;
+	accuracy_test = (count_test / Response_test.rows) * 100;
+	cout << "accuracy_test : " << accuracy_test << endl;
 
 
-	cout << "accuracy : " << accuracy << endl;
-	
+	cout << "\n//////////////////////////////////////////////////////" << endl;
+	cout << "                      svm save                   " << endl;
+	cout << "//////////////////////////////////////////////////////\n" << endl;
+	/// Save svm file
+	svm.save("MNIST_CNN_SVM.xml");
 
 
 }
@@ -193,3 +214,4 @@ void ConvertVectortoMatrix(vector<vector<float> > &trainHOG, vector<vector<float
 		}
 	}
 }
+
